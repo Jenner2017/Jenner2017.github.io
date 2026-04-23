42a5dc7274a4d538712f2b09729a84caf81228b8
jenner <jennercalito2016@gmail.com>
2026-04-22 19:10:05 -0600

    build(angular): relax style budget and remove unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent]
+  imports: [RouterOutlet, FooterComponent]
 })
 export class AppComponent {
   title = 'portfolio-site';
